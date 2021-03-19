dec5e81b441aba67b7e4e717d16d7d81830c0752
GSLOCACAO <gslocacao@MacBook-Pro-de-GSLOCACAO.local>
2021-03-18 22:31:49 -0300

    more animations

--- MemoryGame/MemoryGame/Cardify.swift
+++ MemoryGame/MemoryGame/Cardify.swift
@@ -7,20 +7,33 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var isFaceUp: Bool {
+        return rotation < 90
+    }
+    var rotation: Double
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
     
     func body(content: Content) -> some View {
         ZStack{
-            if isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                content
-            } else {
+                Group {
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    content
+                }
+                    .opacity(isFaceUp ? 1 : 0)
                 RoundedRectangle(cornerRadius: cornerRadius).fill()
-            }
+                    .opacity(isFaceUp ? 0 : 1)
         }
-        //.rotation3DEffect(Angle.degrees(card.isFaceUp ? 0 : 180), axis: (0, 1, 0))
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
     
     private let cornerRadius: CGFloat = 10
--- MemoryGame/MemoryGame/EmojiMemoryGameView.swift
+++ MemoryGame/MemoryGame/EmojiMemoryGameView.swift
@@ -21,7 +21,7 @@ struct EmojiMemoryGameView: View {
                 .padding()
             Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    withAnimation(.linear(duration: 3.5), {
+                    withAnimation(.linear(duration: 1), {
                         viewModel.choose(card: card)
                     })
                 }
@@ -30,7 +30,7 @@ struct EmojiMemoryGameView: View {
             .foregroundColor(viewModel.themeColor)
             .padding()
             Button(action: {
-                withAnimation(.easeInOut(duration: 2)) {
+                withAnimation(.easeInOut(duration: 1)) {
                     viewModel.newGame()
                 }
             }, label: { Text("New Game") })
@@ -51,11 +51,30 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
+                Group{
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                            .onAppear {
+                                self.startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
